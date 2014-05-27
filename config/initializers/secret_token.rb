20f07995df42ac1ab6891a978381921f4f996f5f
duanjs <djs@DonymatoMacBook-Pro.local>
2014-05-27 10:51:17 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'f5eadb579f8cd055096029bb83ea5359b9a9ce841d3d41ed6b541c4283a5da99f59f76d2282e505eab8894cf75b96414440f47f4193a73a4a8f5071ba8cfe811'
